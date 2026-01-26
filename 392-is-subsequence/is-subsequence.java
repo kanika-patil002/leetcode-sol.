adbd73729ffd2a3b91a207b27388db9051cbb488
kanika-patil002 <patilkanika2006@gmail.com>
2026-01-26 12:16:48 +0530

    Time: 2 ms (67.41%) | Memory: 42.9 MB (55.60%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        int index = 0;
+
+        for (int i = 0; i < t.length(); i++) {
+
+            if (index < s.length() && s.charAt(index) == t.charAt(i)) {
+                index++;
+            }
+        }
+
+        return index == s.length();
+    }
+}
